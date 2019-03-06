6883494493276cf2e1f9cdc77b496ec34d3ed45a
pafik13 <re1ax_88@mail.ru>
2019-03-06 23:31:56 +0300

    Reconfig CirlceCI
    - exclude unused scripts
    - add own S3
    - remove singing

--- .circleci/namefiles.sh
+++ .circleci/namefiles.sh
@@ -2,7 +2,7 @@
 set -euvo pipefail
 IFS=$'\n\t'
 
-FILENAME="$ROCKET_DEPLOY_DIR/rocket.chat-$ARTIFACT_NAME.tgz";
+FILENAME="$ROCKET_DEPLOY_DIR/rocket-$CIRCLE_SHA1.tgz";
 
 ln -s /tmp/build/Rocket.Chat.tar.gz "$FILENAME"
-gpg --armor --detach-sign "$FILENAME"
+# gpg --armor --detach-sign "$FILENAME"
