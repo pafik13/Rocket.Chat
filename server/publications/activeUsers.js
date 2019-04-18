4c2c7635d744ecf5148fe750e0e40241274d1af6
pafik13 <re1ax_88@mail.ru>
2019-04-18 16:18:13 +0000

    Task: add more fields in user status change event

--- server/publications/activeUsers.js
+++ server/publications/activeUsers.js
@@ -8,16 +8,43 @@ Meteor.publish('activeUsers', function() {
 		return this.ready();
 	}
 
+	const pub = this;
+
 	const records = Subscriptions.findByUserIdAndType(userId, 'd', { fields: { i: 1 } }).fetch();
 
 	const userIds = records.map((record) => record.i._id);
 
-	return Users.findUsersWithUsernameByIdsNotOffline(userIds, {
+	const options = {
 		fields: {
 			username: 1,
 			name: 1,
 			status: 1,
 			utcOffset: 1,
 		},
+	};
+
+	const cursorHandle = Users.findUsersWithUsernameByIdsNotOffline(userIds, options).observeChanges({
+		added(_id, record) {
+			record.realAction = 'added';
+			return pub.added('users', _id, record);
+		},
+		changed(_id, record) {
+			pub.removed('users', _id, record);
+			const user = Users.findOneById(_id, options);
+			user.realAction = 'changed';
+			return pub.added('users', _id, user);
+		},
+		removed(_id, record) {
+			pub.removed('users', _id, record);
+			const user = Users.findOneById(_id, options);
+			user.realAction = 'removed';
+			return pub.added('users', _id, user);
+		},
+	});
+
+	this.ready();
+
+	this.onStop(function() {
+		return cursorHandle.stop();
 	});
 });
