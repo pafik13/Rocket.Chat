6ed42ebb09834034b4e7c25a5c12f07477ab3389
Lyubin Pavel <lyubin.p@gmail.com>
2019-10-09 16:29:43 +0000

    Fix method name in error (unbclockUser)

--- packages/rocketchat-lib/server/methods/unblockUser.js
+++ packages/rocketchat-lib/server/methods/unblockUser.js
@@ -9,14 +9,14 @@ Meteor.methods({
 		check(blocked, String);
 
 		if (!Meteor.userId()) {
-			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'blockUser' });
+			throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'unblockUser' });
 		}
 
 		const subscription = Subscriptions.findOneByRoomIdAndUserId(rid, Meteor.userId());
 		const subscription2 = Subscriptions.findOneByRoomIdAndUserId(rid, blocked);
 
 		if (!subscription || !subscription2) {
-			throw new Meteor.Error('error-invalid-room', 'Invalid room', { method: 'blockUser' });
+			throw new Meteor.Error('error-invalid-room', 'Invalid room', { method: 'unblockUser' });
 		}
 
 		Subscriptions.unsetBlockedByRoomId(rid, blocked, Meteor.userId());
