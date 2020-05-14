d2bb6f5384a7ca551a90b64f2b16b749b0c7fd0d
Lyubin Pavel <lyubin.p@gmail.com>
2020-05-14 06:59:14 +0000

    Fix lint

--- tests/end-to-end/api/05-direct-message.js
+++ tests/end-to-end/api/05-direct-message.js
@@ -291,7 +291,7 @@ describe('[Direct Messages]', function() {
 			request.post(api('im.create'))
 				.set(credentials)
 				.send({
-					 userId: 'rocket.cat',
+					userId: 'rocket.cat',
 				})
 				.expect('Content-Type', 'application/json')
 				.expect(200)
