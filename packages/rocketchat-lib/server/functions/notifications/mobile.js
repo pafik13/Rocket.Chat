0fe5e9607da118d8ca9ac3a09d92bc9c16676008
Lyubin Pavel <lyubin.p@gmail.com>
2020-10-13 09:01:50 +0000

    Fix url replacement in notification message

--- packages/rocketchat-lib/server/functions/notifications/mobile.js
+++ packages/rocketchat-lib/server/functions/notifications/mobile.js
@@ -3,13 +3,16 @@ import { settings } from 'meteor/rocketchat:settings';
 import { nats } from 'meteor/rocketchat:models';
 import { roomTypes } from 'meteor/rocketchat:utils';
 import { RocketChatAssets } from 'meteor/rocketchat:assets';
+import { getURL } from 'meteor/rocketchat:utils';
+import s from 'underscore.string';
 
 const natsQueue = process.env.NATS_QUEUE || 'notifications';
 
 export function sendSinglePush({ notificationMessage, room, message, sender }) {
 
-
-	notificationMessage = notificationMessage.replace(/\[ \]\(https:\/\/chat\.apianon\.ru\/(d|c|g|p|channel|direct|group|private|public)\/(.)+\)/gm, '');
+	const url = getURL('', { cdn: true, full: true });
+	const regexString = `\\[ \\]\\(${ s.escapeRegExp(url) }(d|c|g|p|channel|direct|group|private|public\\/)(.)+\\) `;
+	const answerRE = new RegExp(regexString, 'gm');
 
 	const { _id, username, name, customFields } = sender;
 
@@ -22,6 +25,6 @@ export function sendSinglePush({ notificationMessage, room, message, sender }) {
 		messageId: message._id,
 		messageType: message.t,
 		host: Meteor.absoluteUrl(),
-		notificationMessage: settings.get('Push_show_message') ? notificationMessage : ' ',
+		notificationMessage: settings.get('Push_show_message') ? notificationMessage.replace(answerRE, '') : ' ',
 	});
 }
