6cfeba18aa30c92e095716bf7332f098fef5e6e6
Lyubin Pavel <lyubin.p@gmail.com>
2020-10-20 22:14:48 +0000

    Fix test

--- tests/end-to-end/api/16-admin.js
+++ tests/end-to-end/api/16-admin.js
@@ -50,7 +50,8 @@ describe('[Admin]', function() {
 			.expect(400)
 			.expect((res) => {
 				expect(res.body).to.have.property('success', false);
-				expect(res.body).to.have.property('error', 'You must be a admin!');
+				expect(res.body).to.have.property('error', 'You must be a admin! [error-access-denied]');
+				expect(res.body).to.have.property('errorType', 'error-access-denied"');
 			})
 			.end(done));
 
