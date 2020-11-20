0c92d293a5abd848236c4b40258a4577faa8eec9
Lyubin Pavel <lyubin.p@gmail.com>
2020-11-20 07:17:02 +0000

    Fix response code in users.update when user not found

--- packages/rocketchat-api/server/v1/users.js
+++ packages/rocketchat-api/server/v1/users.js
@@ -381,7 +381,7 @@ API.v1.addRoute('users.update', { authRequired: true }, {
 		});
 		const { userId } = this.bodyParams;
 		const user = Users.findOneById(userId, { fields: { customFields: 1 } });
-		if (!user) { return API.v1.failure('User not found'); }
+		if (!user) { return API.v1.notFound('User not found'); }
 		const userData = _.extend({ _id: userId }, this.bodyParams.data);
 
 		Meteor.runAsUser(this.userId, () => saveUser(this.userId, userData));
