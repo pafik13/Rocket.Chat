bd7b7e6888ea7b13e7767796e72b3244e55f023f
pafik13 <re1ax_88@mail.ru>
2020-11-27 19:42:44 +0300

    Add getCountriesWithSameLanguage (one more file)

--- packages/rocketchat-utils/server/index.js
+++ packages/rocketchat-utils/server/index.js
@@ -24,6 +24,7 @@ export { stringToBoolean } from '../lib/stringToBoolean';
 export { hyphenate } from '../lib/hyphenate';
 export { complaintReasonsList } from '../lib/complaintReasons';
 export { randomInteger } from './functions/randomInteger';
+export { getCountriesWithSameLanguage } from './functions/getCountriesWithSameLanguage';
 
 export const HEADER_COUTNRY_CODE = 'X-Country-Code';
 export const HEADER_NGINX_GEO_CODE = 'X-Nginx-Geo-Client-Country';
